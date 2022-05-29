e5232c27fc9a7064257c4cc2539fe5baf55207b7
Omar Mohamed <100616092+omarlawatey@users.noreply.github.com>
2022-05-29 13:28:03 +0200

    Big Babel Fix

--- src/functions/ServerStatusUpdate.js
+++ src/functions/ServerStatusUpdate.js
@@ -3,12 +3,12 @@ import { checkLiveStatus } from '../assets/subFunctions';
 
 const ServerStatusUpdate = (serverInfo, guild) => {
   serverInfo.liveStatus.Roles.forEach(async liveUpdate => {
-    let role = guild.roles.cache.get(liveUpdate.id);
-    let channel = guild.channels.cache.get(
+    let role = guild?.roles.cache.get(liveUpdate.id);
+    let channel = guild?.channels.cache.get(
       await checkLiveStatus(serverInfo, guild, role ? role : { name: 'notFound', id: '0' })
     );
 
-    await channel.setName(`${fontGenerator(serverInfo, liveUpdate.name)}: ${role.members.map(i => i.name).length}`);
+    await channel?.setName(`${fontGenerator(serverInfo, liveUpdate.name)}: ${role.members.map(i => i.name).length}`);
   });
 };
 
